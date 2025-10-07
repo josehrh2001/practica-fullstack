5cc7570da8c6a6789f2df06e0e75ebe1f27e947d
josephrh001 <josephemmanuelrios2001@gmail.com>
2025-10-07 11:19:29 -0600

    Se cargo el proyecto base Spring Boot

---
+++ backend/practica/src/main/java/com/practica/fullstack/PracticaFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.practica.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaFullstackApplication.class, args);
+	}
+
+}
