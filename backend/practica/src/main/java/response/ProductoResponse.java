1febafbeb8538f42c6605099d364ae55ffbd5f03
josephrh001 <josephemmanuelrios2001@gmail.com>
2025-10-07 16:06:42 -0600

    Clases de metadata para el crud

---
+++ backend/practica/src/main/java/response/ProductoResponse.java
@@ -0,0 +1,11 @@
+package response;
+
+import com.practica.fullstack.model.Producto;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ProductoResponse {
+    private List<Producto> producto;
+}
---
+++ backend/practica/src/main/java/response/ProductoResponseRest.java
@@ -0,0 +1,13 @@
+package response;
+
+import com.practica.fullstack.model.Producto;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ProductoResponseRest {
+    private ProductoResponse productoResponse = new ProductoResponse();
+}
